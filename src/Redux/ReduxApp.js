58b0cf53e65ed46fb5fa4dfe468df9c734346651
vardhan <harshwardhankalse03@gmail.com>
2021-07-14 22:50:33 +0530

    Adding Component of New IceCreamContainer in Redux App

--- src/Redux/ReduxApp.js
+++ src/Redux/ReduxApp.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CakeContainer from "./Components/CakeContainer";
 import CakeContainerHooks from "./Components/CakeContainerHooks";
+import IceCreamContainer from "./Components/IceCreamContainer";
 
 const ReduxApp = () => {
   return (
     <div>
       <CakeContainer />
       <CakeContainerHooks />
+      <IceCreamContainer />
     </div>
   );
 };
