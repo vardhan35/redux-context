929f51cd5cec810759e834f47112a696fe92fee4
vardhan <harshwardhankalse03@gmail.com>
2021-07-15 14:11:22 +0530

    Adding userReducer to rootReducer

--- src/Redux/redux/rootReducer.js
+++ src/Redux/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import cakeReducer from "./cake/CakeReducer";
 import CreamReducer from "./IceCream/CreamReducer";
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   cream: CreamReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
