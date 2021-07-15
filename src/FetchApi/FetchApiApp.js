20d21d262d410daca162504777a1bdeb31a53895
vardhan <harshwardhankalse03@gmail.com>
2021-07-15 21:06:01 +0530

    Adding FetchApi Component to FetchApiApp.js

--- src/FetchApi/FetchApiApp.js
+++ src/FetchApi/FetchApiApp.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Axios from "./Components/Axios";
+import FetchApi from "./Components/FetchApi";
 
 const FetchApiApp = () => {
   return (
     <div>
       <Axios />
+      <FetchApi />
     </div>
   );
 };
