15464a11a16cf2d662e88d598c997191e484a562
vardhan <harshwardhankalse03@gmail.com>
2021-07-15 14:22:40 +0530

    Adding fetchuser function in userActions

--- src/Redux/redux/user/userActions.js
+++ src/Redux/redux/user/userActions.js
@@ -4,6 +4,8 @@ import {
   FETCH_USERS_FAILURE,
 } from "./userActionsType";
 
+import axios from "axios";
+
 export const fetchUsersRequest = () => {
   return {
     type: FETCH_USERS_REQUEST,
@@ -21,5 +23,21 @@ export const fetchUsersFailure = (err) => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: err,
+  };
+};
+
+export const fetchUsers = () => {
+  return (dispatch) => {
+    dispatch(fetchUsersRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data;
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((err) => {
+        const errMsg = err.message;
+        dispatch(fetchUsersFailure(errMsg));
+      });
   };
 };
