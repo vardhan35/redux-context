f792b8d4b8d3c43c2f837375c80a0ae4167f6b75
vardhan <harshwardhankalse03@gmail.com>
2021-07-15 14:22:17 +0530

    importing axios and thunk in store

--- src/Redux/redux/Store.js
+++ src/Redux/redux/Store.js
@@ -2,9 +2,10 @@ import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
 export default store;
