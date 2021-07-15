a675c1eb34ec4bbd7253e4428b19ef42fa233fb5
vardhan <harshwardhankalse03@gmail.com>
2021-07-15 14:40:28 +0530

    dispatching fetchUsers function and using state in UserContainer Component by using mapStateToprops, mapDispatchTo Props

--- src/Redux/Components/UserContainer.js
+++ src/Redux/Components/UserContainer.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect, useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "../redux/user/userActions";
 
-const UserContainer = () => {
-  return <h1>Hello World From UserContainer</h1>;
+const UserContainer = ({ fetchUserData, userData }) => {
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+  return userData.loading ? (
+    <h2>Loading</h2>
+  ) : userData.err ? (
+    <h2>{userData.err}</h2>
+  ) : (
+    <div>
+      <h2>User List</h2>
+      <div>
+        {userData &&
+          userData.users &&
+          userData.users.map((user) => <p key={user.id}>{user.name}</p>)}
+      </div>
+    </div>
+  );
 };
 
-export default UserContainer;
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUserData: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
