7f1406c526b3306c07b198dda1fa6482b3e71ca4
vardhan <harshwardhankalse03@gmail.com>
2021-07-16 19:47:44 +0530

    UseCallBack.js Component Added to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import UseCallBackApp from "./Callback/UseCallBackApp";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UseCallBackApp />
+    </div>
+  );
 }
 
 export default App;
