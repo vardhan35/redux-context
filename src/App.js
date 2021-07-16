5dd139fb5647940593f5e318fc5c9d051968bb6d
vardhan <harshwardhankalse03@gmail.com>
2021-07-16 20:43:45 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+<<<<<<< HEAD
 <<<<<<< .merge_file_a02688
 import ContextApp from "./Context/ContextApp";
 =======
 import FetchApiApp from "./FetchApi/FetchApiApp";
 >>>>>>> .merge_file_a13436
+=======
+import ContextApp from "./Context/ContextApp";
+>>>>>>> context
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
 <<<<<<< .merge_file_a02688
       <ContextApp />
 =======
       <FetchApiApp />
 >>>>>>> .merge_file_a13436
+=======
+      <ContextApp />
+>>>>>>> context
     </div>
   );
 }
