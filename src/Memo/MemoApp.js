6a643c9d876493f5b27fef09392a977a8230e9eb
vardhan <harshwardhankalse03@gmail.com>
2021-07-16 20:23:40 +0530

    Added usememo hook to our app

--- src/Memo/MemoApp.js
+++ src/Memo/MemoApp.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMemo } from "react";
 
 const MemoApp = () => {
-  return <h1>This is memo App</h1>;
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const increment1 = () => {
+    setCount1(count1 + 1);
+  };
+
+  const increment2 = () => {
+    setCount2(count2 + 1);
+  };
+
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 200000000) i++;
+    return count1 % 2 === 0;
+  }, [count1]);
+
+  return (
+    <div>
+      <div>
+        <button onClick={increment1}>Increment1 - {count1} </button>
+        {isEven ? "Even" : "Odd"}
+      </div>
+      <div>
+        <button onClick={increment2}>Increment2 - {count2} </button>
+      </div>
+    </div>
+  );
 };
 
 export default MemoApp;
